d256f327548e7a01af5ce15648d07087c85626b2
Dillon <dilloncortez@gmail.com>
2025-03-31 15:58:37 -0500

    updated origin urls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://dillon-co.github.io', 'https://letsresonate.live', 'http://localhost:3000', 'http://localhost:5173'
+    origins 'https://dillon-co.github.io', 'https://letsresonate.live', 'http://localhost:3000', 'http://localhost:5173', 'https://resonate-backend-0fac85455b6b.herokuapp.com'
     
     resource '*',
       headers: :any,
